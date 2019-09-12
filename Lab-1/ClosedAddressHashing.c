7c035383dc1eaaeffdf9b84f5301c9cb1f7d5a8d
Jay Gupta <43084501+guptajay@users.noreply.github.com>
2019-09-12 17:02:32 +0800

    Update ClosedAddressHashing.c
    Added No. of Key Comparisons & CPU Time

--- Lab-1/ClosedAddressHashing.c
+++ Lab-1/ClosedAddressHashing.c
@@ -6,6 +6,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 // Define number of slots = h
 // number of elements = n
@@ -32,7 +33,7 @@ void insert(int value)
     // key = value mod h
     int key = value % h;
 
-    // Checking if chain[key] is empty
+    // Checking if slot[key] is empty
     if(slot[key] == NULL)
         // if empty, create a new node to the corresponding slot
         slot[key] = newNode;
@@ -40,7 +41,7 @@ void insert(int value)
     // If it is going to the else part, then, it is a collision
     else
     {
-        // Add the node at the end of chain of the corresponding slot
+        // Add the node at the end of slot of the corresponding slot
         struct node *temp = slot[key];
         // iterate to the last node in the linked list
         while(temp->next)
@@ -69,8 +70,41 @@ void print()
     }
 }
 
+void search(int item) {
+
+    // Calculating Key
+    int key = item % h;
+    int no_of_key_comparisons = 0;
+    int flag = 0;
+
+    struct node *temp1 = slot[key];
+
+    while(temp1 != NULL) {
+
+        no_of_key_comparisons++;
+
+        if (temp1->data == item) {
+            flag = 1;
+            printf("\n%d is found in Slot %d!", item, key);
+            printf("\nNumber of Key Comparisons Made = %d\n", no_of_key_comparisons);
+            return;
+        }
+        temp1 = temp1->next;
+    }
+
+    if(flag == 0) {
+        printf("\n%d Not Found!\n");
+        printf("\nNumber of Key Comparisons Made = %d\n", no_of_key_comparisons);
+        return;
+    }
+}
+
 int main()
 {
+    clock_t t1, t2;
+
+    // Program Starting
+    t1 = clock();
     int i;
 
     // initializing the whole array to NULL
@@ -84,7 +118,23 @@ int main()
     insert(4);
     insert(5);
 
+    t1 = clock() - t1;
+    // Program Ending
+
+    // Exclude printing time in CPU Time
     print();
+
+    // Program Starting
+    t2 = clock();
+
+    search(10);
+
+    t2 = clock() - t2;
+    // Program Ending
+
+    double time_taken = ((double)t1)/CLOCKS_PER_SEC + ((double)t2)/CLOCKS_PER_SEC; // in seconds
+
+    printf("CPU Time = %f\n", time_taken);
 
     return 0;
 }
